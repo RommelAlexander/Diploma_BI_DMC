0854a4e3343a31387db9a6361d029fb77e892091
RommelAlexander <alexanderpicon11@gmail.com>
2026-06-03 19:58:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import streamlit as st
+st.title("Proyecto fina de Diploma BI")
+st.ssidebar.title("Parametros")
 
 
 
 
 
+st.write("Elaborado por: Rommel Picon")
 
-
-
-
-
-archivo = st.file_uploader("Cargue el archivo excel)
+archivo = st.file_uploader("Cargue el archivo excel o csv")
+if archivo is not None:
+  
+  if archivo.nameednswith(".csv"):
+    data= pd.read_csv(archivo)
+    st.writre(data)
+  elif archivo.nameednswith(".xlsx"):
+    data= pd.read_excel(archivo)
+    st.writre(data)
+  else
+  st.write("Formato no valido")
+  
+  else:
+  st.write("Por favor cargue su archivo")
